d7f4746f43c9c59bdc256664e18f5a0a41c8469c
YarynaPelekh <yaryna.pelekh@hebronsoft.com>
2022-06-17 18:56:26 +0300

    refactored Catd.tsx

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,11 @@
+import classes from "./Card.module.css";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Card: React.FC<Props> = (props) => {
+  return <div className={classes.card}>{props.children}</div>;
+};
+
+export default Card;
